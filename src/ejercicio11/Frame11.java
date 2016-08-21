f200de4d6183dc9df51c0b9a2e553135f6eff454
jparedes2 <paredesjorge10@hotmail.com>
2016-08-20 21:20:39 -0500

    Interfaz grafica y ejercicio 11 TERMINADO

--- src/ejercicio11/Frame11.java
+++ src/ejercicio11/Frame11.java
@@ -5,6 +5,8 @@
  */
 package ejercicio11;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author pared
@@ -27,21 +29,187 @@ public class Frame11 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        txtMontoAnual = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        txtGineco = new javax.swing.JTextField();
+        txtTraumato = new javax.swing.JTextField();
+        txtPedriat = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel1.setText("Hospital");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 10, -1, -1));
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel2.setText("Monto Presupuesto Anual:");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 60, -1, -1));
+
+        txtMontoAnual.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtMontoAnualKeyTyped(evt);
+            }
+        });
+        jPanel1.add(txtMontoAnual, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 60, 130, -1));
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel3.setText("Ginecologia (40%):");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 100, -1, -1));
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel4.setText("Traumatologia (30%):");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 130, -1, -1));
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel5.setText("Pedriatia (30%):");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 160, -1, -1));
+
+        txtGineco.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtGinecoKeyTyped(evt);
+            }
+        });
+        jPanel1.add(txtGineco, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 100, 130, -1));
+
+        txtTraumato.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtTraumatoKeyTyped(evt);
+            }
+        });
+        jPanel1.add(txtTraumato, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 130, 130, -1));
+
+        txtPedriat.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtPedriatKeyTyped(evt);
+            }
+        });
+        jPanel1.add(txtPedriat, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 160, 130, -1));
+
+        jButton1.setText("Ejecutar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 230, -1, -1));
+
+        jButton2.setText("Borrar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 230, -1, -1));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 560, 410));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        long montanual, ginecol=40, traumatol=30, piedrati=30, op1, op2, op3;
+        String res1, res2, res3;
+        if (txtMontoAnual.getText().trim().isEmpty())
+        {
+            JOptionPane.showMessageDialog(this, "Ingrese el Monto", "Error", JOptionPane.ERROR_MESSAGE);
+            txtMontoAnual.requestFocusInWindow();
+        }
+        else {
+        montanual= Long.parseLong(txtMontoAnual.getText());
+        
+        op1 = (ginecol*montanual)/100;
+        op2= (traumatol*montanual)/100;
+        op3= (piedrati*montanual)/100;
+        
+        res1= String.valueOf(op1);
+        res2= String.valueOf(op2);
+        res3 = String.valueOf(op3);
+        txtGineco.setText(res1);
+        txtPedriat.setText(res3);
+        txtTraumato.setText(res2);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void txtMontoAnualKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtMontoAnualKeyTyped
+        // TODO add your handling code here:
+        char c=evt.getKeyChar(); 
+          if(!Character.isDigit(c)) 
+          { 
+              getToolkit().beep(); 
+              evt.consume();      
+          }
+         
+         
+    }//GEN-LAST:event_txtMontoAnualKeyTyped
+
+    private void txtGinecoKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtGinecoKeyTyped
+        // TODO add your handling code here:
+        char c=evt.getKeyChar(); 
+          if(!Character.isDigit(c)) 
+          { 
+              getToolkit().beep(); 
+              evt.consume();      
+          }
+         
+          if(!Character.isLetter(c)) 
+          { 
+              getToolkit().beep(); 
+              evt.consume(); 
+          }
+    }//GEN-LAST:event_txtGinecoKeyTyped
+
+    private void txtTraumatoKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtTraumatoKeyTyped
+        // TODO add your handling code here:
+        char c=evt.getKeyChar(); 
+          if(!Character.isDigit(c)) 
+          { 
+              getToolkit().beep(); 
+              evt.consume();      
+          }
+         
+          if(!Character.isLetter(c)) 
+          { 
+              getToolkit().beep(); 
+              evt.consume(); 
+          }
+    }//GEN-LAST:event_txtTraumatoKeyTyped
+
+    private void txtPedriatKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtPedriatKeyTyped
+        // TODO add your handling code here:
+        char c=evt.getKeyChar(); 
+          if(!Character.isDigit(c)) 
+          { 
+              getToolkit().beep(); 
+              evt.consume();      
+          }
+         
+          if(!Character.isLetter(c)) 
+          { 
+              getToolkit().beep(); 
+              evt.consume(); 
+          }
+    }//GEN-LAST:event_txtPedriatKeyTyped
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        txtGineco.setText("");
+        txtPedriat.setText("");
+        txtTraumato.setText("");
+        txtMontoAnual.setText("");
+        
+        txtMontoAnual.requestFocusInWindow();
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +247,17 @@ public class Frame11 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField txtGineco;
+    private javax.swing.JTextField txtMontoAnual;
+    private javax.swing.JTextField txtPedriat;
+    private javax.swing.JTextField txtTraumato;
     // End of variables declaration//GEN-END:variables
 }
